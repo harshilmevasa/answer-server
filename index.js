488c313bfbbfd6f5dbc19974f242e6c1eb38e4da
harshilmevasa <30534913+harshilmevasa@users.noreply.github.com>
2021-04-27 00:49:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.use('/api/companies', companyRoutes);
 
 app.use('/api/forgot-password', forgotPassword);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Server started');
 });
